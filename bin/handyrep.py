8202da979dd9d72487b47cf85555245a52386dd9
Josh Berkus <josh@pgexperts.com>
2013-10-02 16:18:03 -0700

    Added new doc.  Added skeleton python file.

---
+++ bin/handyrep.py
@@ -0,0 +1,151 @@
+
+
+class HandyRep(Object):
+
+    def __init__(config_file='handyrep.conf', server_file='servers.save'):
+        # open the config file
+        # validate the file
+        # open the current servers file
+            # create if not present
+            # validate if present
+        # open the handyrep table on the master
+            # create if not present
+            # if handyrep table more updated
+            # use that
+        # return a handyrep object
+
+    def check_my_status(self):
+        # check plugin method to see
+        # return result
+
+    def verify_servers(self):
+        # check each server definition against
+        # the reality
+        # update server definitions if required
+        # return error if serverdefs don't match
+        # succes otherwise
+
+    def sync_config(self):
+        # open the handyrep table on the master
+            # create if not present
+        # if handyrep table more updated
+            #supercede local servers.save
+        # otherwise, save serverinfo to table
+        # save all files
+        # return true if changes synced
+
+    def check_master(self):
+        # check master using poll method
+        # if false, try to restart master
+        # otherwise, poll again after retry_interval
+        # if failed retry_times, raise error
+
+    def check_replica(self, replicaserver):
+        # check replical using poll method
+        # check that it's in replication
+        # check that it's seen from master
+        # check replica lag
+        # if any checks fail, error
+        # otherwise, return success
+
+    def get_server_status(self, servername=None):
+        # if no server given, loop through all
+        # get master info
+        # for each replica:
+            # check that replica is up and replicating
+            # if replica isn't replicating, raise error and change status
+            # check replica lag
+        # return all status info
+
+    def failover_check(self):
+        # check master
+        # if failed, failover
+        # return 
+
+    def failover(self, newmaster=None, remaster=None):
+        # if newmaster isnt set, poll replicas for new master
+        # according to selection_method
+        # if remaster not set, get from settings
+        # attempt STONITH
+            # if failed, abort and reset
+        # attempt replica promotion
+            # if failed, try to abort and reset
+            # if success, update servers.save
+        # if remastering, attempt to remaster
+        # update servers.save
+        # run post-failover scripts
+
+    def stonith(self, oldmaster=None):
+        # get master from serversettings if not supplied
+        # test if we can ssh to master
+            # check if PG is running
+            # if so, run shutdown
+            # if shutdown fails, error out
+        # test if master IP is up
+
+    def promote(self, newmaster):
+        # test access to new master
+        # can we ssh?
+        # is it running?
+        # is it a replica?
+        # if all true, send promote command
+        # return true if successful, otherwise error
+
+    def remaster(self, replicaserver, newmaster=None):
+        # use master from settings if not supplied
+        # change replica config
+        # restart replica
+        # check for fail
+        # sync server config
+
+    def clone(self, replicadict, clonefrom=None, masterserver=None, force=False):
+        # use config master if not supplied
+        
+
+    def reclone(self, replicaserver, clonefrom=None):
+        # check server config
+        # shutdown replica, if required
+
+    def get_server_info(self, servername=None, format="json", sync=True):
+        # formats: json, yaml
+        # if sync:
+            # verify_servers
+            # sync_config if changed
+        # if all, return all servers
+        # otherwise return just the one
+
+    def get_server_role(self, serverrole, format="json", sync=True):
+        # formats: json, yaml
+        # roles: master, replicas
+        # if sync:
+            # verify_servers
+            # sync_config if changed
+        # return master if master
+        # if replicas, return all replicas
+
+    def validate_server_settings(self, servername, serverdict):
+        # check each setting against the existing servers
+        # check for possible, rather than current
+
+    def change_server(self, servername, serverdict, isnew=false):
+        # verify servers
+        # validate new settings
+        # check for settings we don't do, like recloning
+        # check each setting against the existing settings for the server
+        # for changed settings & new servers
+            # does the setting need to be pushed to the server?
+            # if so, push it to the server
+            # check for success; error out on fail
+        # sync server config
+        # exit with success
+
+    def clean_archive(self, expire_hours=None, archivedict=None):
+        # if not archive details, then
+        # use config
+        # delete files from archive which are older
+        # than setting
+
+            
+
+
+        
---
+++ docs/designspec.rst
@@ -0,0 +1,102 @@
+HandyRep
+========
+
+General
+-------
+
+* Written entirely in Python
+* Intended to be run on a single server (not on each server in the cluster)
+* Can be run from a 3rd server (such as a pgbouncer server)
+* Can be run in a failover configuration, with a 2nd Handyrep which doesn't accept commands.
+* Designed to minimize the per-node installation requirements.
+* Controls one single PostgreSQL cluster of master-slave replicas
+* Binary replication only
+* Supports multiple replicas
+* Supports cascading replication
+* Only one (network) location for archiving
+* Supporting only PostgreSQL 9.2 and later
+* Does not deal with PITR, except to support WAL-E archiving
+
+Setup Requirements
+------------------
+
+On Handyrep Server:
+* python 2.7+, plus fabric, paramiko, pyyaml, psycopg2, and uWSGI
+* ssh and rsync
+* ssh keys to all servers as user with sudo postgres and root
+* postgres password
+
+Optional (on Handyrep Server):
+* pg_isready from PostgreSQL 9.3
+* pgbouncer, pgpool2 or HAProxy
+* ssh keys to archive location
+
+On Nodes:
+* PostgreSQL
+* sudo
+* postgres port open to HandyRep server
+* directory controlled by handyrep user and accessible to postgres user
+
+Optional on Nodes:
+* WAL-E and/or archive scripts
+
+Configuration
+-------------
+
+* Two YAML configuration files: handyrep.conf and servers.save
+* Handyrep.conf represents initial default config and general config
+* Servers.save is generated and written by the runtime, and is automatically updated with current server information.
+* Both configuration files are written to the database in a table called handyrep.
+* Configuration files are timestamped to figure out "latest".
+
+Plug-Ins
+--------
+
+* users can write plugin methods for various tasks: server check, cloning, new master selection, extra failover commands
+* these plugins are python code added to plugins.py
+* they can then be added to the config
+
+Daemon
+------
+
+* HandyRep will run as a uWSGI daemon/server.
+* The web service will require simple authentication
+* It will take HandyRep commands by POST
+* It will supply data when polled in JSON format
+
+Polling and Automated Failover
+------------------------------
+
+* If configured, every poll_interval HandyRep will poll the master for uptime.
+* If the master doesn't respond using poll_method in poll_tries, HandyRep will intiate failover.
+* New master will be determined by selection_method
+* HandyRep will try to fail over to new master or roll back on fail
+* If configured, HandyRep will also try to remaster other replicas.  It will NOT rollback if these fail.
+* Will also execute post-failover_commands as configured.
+
+Actions Supported by HandyRep
+-----------------------------
+
+* initialize
+* check handyrep status
+* verify servers (against config)
+* sync configuration
+* check master
+* check replica(s)
+* get server(s) status
+* failover check
+* failover
+* stonith (the master)
+* promote (a replica)
+* remaster (a replica)
+* reclone (a replica)
+* get server info (current config)
+* get server by role (master, replica)
+* validate server settings (against correct format)
+* change server (new config)
+* clean archive (of old files)
+
+
+
+
+
